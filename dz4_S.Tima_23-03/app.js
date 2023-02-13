543a03f6ecd63b6fe6eb45e9a94b49121365a484
tima250z <124433487+tima250z@users.noreply.github.com>
2023-02-13 17:54:15 +0600

    Add files via upload

--- dz4_S.Tima_23-03/app.js
+++ dz4_S.Tima_23-03/app.js
@@ -1,25 +1,6 @@
-var arrays = [null, 123, undefined, null, 'qwerty', 34, 65, undefined, null, null, 43, 'aerg', 'пкцйк']
-var str = []
-var nll = []
-var num = []
-var und = []
-
-for (var i = 0 ; i < arrays.length; i++){
-    str.push(arrays[i])
-    var  a = str.filter(r => {return typeof (r) === 'string'})
-    num.push(arrays[i])
-    var b = num.filter(h => {return typeof (h) === 'number'})
-    nll.push(arrays[i])
-    var c = nll.filter(v => {return typeof (v) === 'object'})
-    und.push(arrays[i])
-    var d = und.filter(l => {return typeof (l) === 'undefined'})
+var arr = ['name', 'John', 'lastname', 'Black', 'age', '23'];
+var obj = {};
+for (var i = 0; i < arr.length; i += 2) {
+    obj[arr[i]] = arr[i + 1];
 }
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
-
-var sortArray = []
-sortArray.push(a,b,c,d)
-sortArray.sort((a,b) => {return b.length - a.length})
-console.log(sortArray)
+console.log(obj);
